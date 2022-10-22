3c60c7eafe6ac6304b78dfaf86fdbabc798175f3
Marshal-Emanuel <marshcodes@gmail.com>
2022-10-22 20:45:26 +0300

    prog 2

---
+++ c trial.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+ int main()
+ {
+     int age;
+     char name;
+     printf("Enter your name");
+     scanf("%c", &name);
+     printf("\nEnter your age:");
+     scanf("%d", &age);
+
+     return 0;
+ }
