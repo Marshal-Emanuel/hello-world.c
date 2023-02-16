b6ce959c3444979beb1d78e12a1447386ccef264
Marshal-Emanuel <marshcodes@gmail.com>
2023-02-16 16:59:14 +0300

    getters fine

---
+++ GetterSetterTry.java
@@ -0,0 +1,39 @@
+public class GetterSetterTry {
+    private String name;
+    private int age;
+    private String level;
+
+    public void setName(String name){
+        this.name=name;        
+    }
+    
+    public String getName(){
+        return name;
+    }
+
+    public void setAge(int age){
+        this.age=age;        
+    }
+    
+    public int getAge(){
+        return age;
+    }
+
+    public String getLevel(){
+        return level;
+    }
+    public void setLevel(String level){
+        this.level=level;
+    }
+//Constructor for this class
+    public GetterSetterTry(String name, int age, String level){
+        this.name=name;
+        this.level=level;
+        this.level=level;
+    }
+
+    
+    }
+    
+
+
---
+++ GetterTwo.java
@@ -0,0 +1,10 @@
+class GetterTwo {
+   
+    public static void main(String[] args) {
+        GetterSetterTry one=new GetterSetterTry("Emanuel", 20, "Campus");
+
+        one.setName("Emanuel");
+
+        System.out.println(one.getName());
+    }
+}
