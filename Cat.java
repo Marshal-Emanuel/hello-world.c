442c1fc8168b3d66e73911eda9c682e33e781afe
Emanuel Marshal <97548460+Marshal-Emanuel@users.noreply.github.com>
2023-08-08 16:29:38 +0300

    Update Cat.java

--- Cat.java
+++ Cat.java
@@ -3,7 +3,7 @@ class Cat extends Animals{
     int age;
     public Cat(String name, String color, int age) {
         super(name, color);
-        //TODO Auto  
+        //TODO Auto 
     }
 
     
